f87472f28a81c1cf3f0e4876392b60e4eb0b4b87
Tatiana Hernandez Villanueva <thernandez@scio.local>
2019-03-08 14:59:29 -0600

    Tarea de pruebas unitarias

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbd9d1b1-bb6f-4fa4-848d-651b5dc7e505")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace UnitTest
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        [DataRow("jlarreygue@sciodev.com", true)]
+        [DataRow("jlarreygue@sciodev", true)]
+        [DataRow("jlarreygue@s@ciodev.com", false)]
+        [DataRow("@sciodev.com", false)]
+        [DataRow("jlarreygue@sciodev.com.com", true)]
+        [DataRow("jlarreyguesciodev.com", false)]
+        [DataRow("jlarreygue@", false)]
+        [DataRow("jlarrey_gue@sciodev.com", true)]
+        [DataRow("jlarreygue@scio_dev.com", true)]
+
+        public void testValidateEmail(String email, bool res)
+        {
+
+            var exp_reg = @"^[a-zA-Z0-9_\-\.]{2,}@[a-zA-Z0-9_\-\.]{2,}$";
+
+            Regex regex = new Regex(exp_reg);
+
+            Assert.AreEqual(regex.IsMatch(email), res);
+        }
+    }
+}
